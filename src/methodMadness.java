1f01ac6ac24d909f51a70b34b9a73fedcf3e9dcb
sajarindider <sajarindider@gmail.com>
2017-10-09 19:34:13 -0400

    Sajarin's class

---
+++ src/methodMadness.java
@@ -0,0 +1,33 @@
+// Created by Sajarin Dider
+// Last Update: 9/28/17
+// Ap Computer Science Java
+// Period 2
+
+
+public class methodMadness{
+
+    public static void main(String[] args) {
+        dateStr();
+        sumUpTo(7);
+        vigCypher();
+    }
+
+    public static String dateStr(){
+
+        return ("Hey");
+    }
+
+    public static int sumUpTo(int n){
+
+        int sum = (n*(n + 1))/ 2;
+        System.out.println(sum);
+        return sum;
+
+    }
+
+    public static int vigCypher(){
+
+
+        return (1);
+    }
+}
---
+++ src/runner.java
@@ -0,0 +1,6 @@
+public class runner {
+
+    public static void main(String[] args) {
+        methodMadness.sumUpTo(1000);
+    }
+}
